a10107c8e565cd528298bb9420a8725d3d71fe7a
mrvnre <mrvnre24@gmail.com>
2020-04-26 11:46:07 -0500

    completed lesson-2-Studio: Counting Characters

--- src/org/launchcode/java/studios/areaofacircle/CountingCharacters.java
+++ src/org/launchcode/java/studios/areaofacircle/CountingCharacters.java
@@ -1,25 +1,26 @@
 package org.launchcode.java.studios.areaofacircle;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CountingCharacters {
     public static void main (String[] args) {
         String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. " +
                 "So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. " +
                 "Once you’ve done that, it’s pretty straightforward from there.";
-        String[] letter = quote.split("");
+        char[] quoteArray = quote.toCharArray();
 
-        ArrayList<String> letters = new ArrayList<>();
-        ArrayList<Integer> counts = new ArrayList<>();
-
-        for (int i=0; i<quote.length(); i++) {
-            if (!letters.contains(letter[i])) {
-                letters.add(letter[i]);
+        HashMap<Character, Integer> countLetters = new HashMap<>();
+        for (char c : quoteArray) {
+            if (countLetters.containsKey(c)) {
+                countLetters.put(c, countLetters.get(c)+1);
+            } else {
+                countLetters.put(c, 1);
             }
         }
-
-        System.out.println(letters);
+        for (Map.Entry<Character, Integer> entry : countLetters.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
 
 
     }
