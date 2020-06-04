62799414479fe96ccfafa1c44238ce684164a97c
mrvnre <mrvnre24@gmail.com>
2020-06-04 16:24:08 -0500

    changes saved

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,7 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student mervenur = new Student("Mervenur", 01171007, 1,4.00);
+        System.out.println(mervenur.studentInfo());
     }
 }
