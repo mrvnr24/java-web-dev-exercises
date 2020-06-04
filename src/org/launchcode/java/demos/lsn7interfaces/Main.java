ed84365fbd60f014d35e95c71f7bb10607892fa4
mrvnre <mrvnre24@gmail.com>
2020-06-04 16:28:10 -0500

    added main class

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,6 +1,7 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
@@ -10,9 +11,16 @@ public class Main {
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
+        Comparator comparator = new FlavorComparator();
+
+        flavors.sort(comparator);
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
+        Comparator comparator1 = new ConeComparator();
+
+        cones.sort(comparator1);
+        System.out.println(cones);
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
     }
