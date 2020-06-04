6eb2156d1024301c644b019721de5a64b2a6e055
mrvnre <mrvnre24@gmail.com>
2020-06-04 16:33:58 -0500

    Added main class

--- src/org/launchcode/java/demos/lsn6inheritance/Main.java
+++ src/org/launchcode/java/demos/lsn6inheritance/Main.java
@@ -1,4 +1,16 @@
 package org.launchcode.java.demos.lsn6inheritance;
 
 public class Main {
+    public static void main(String[] args) {
+        HouseCat garfield = new HouseCat("Garfield", 12);
+        garfield.eat();
+        System.out.println(garfield.isTired());
+
+        HouseCat spike = new HouseCat("Spike");
+        System.out.println(spike.getWeight());
+
+        Cat plainCat = new Cat(8.6);
+        HouseCat cheshireCat = new HouseCat("Chechire", 12); System.out.println(plainCat.noise()); System.out.println(cheshireCat.noise());
+    }
 }
+
